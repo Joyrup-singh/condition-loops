db2a97ac9b07d87dadfe622171ebf4dc6de36a1f
Joyrup Singh <145583757+Joyrup-singh@users.noreply.github.com>
2023-12-01 13:31:47 +0530

    Add files via upload

---
+++ assg19q2.js
@@ -0,0 +1,14 @@
+const marks = 90;
+if(marks >= 90) {
+  grade = "A";
+}
+else if(marks >= 70) {
+  grade = "B";
+}
+else if (marks >= 50) {
+  grade = "C";
+}
+else {
+  grade = "F"
+}
+console.log(' You get the grade ' + grade);
---
+++ assg19q4.js
@@ -0,0 +1,5 @@
+function getRandom(min, max){
+    let x = Math.floor(Math.random() * (max - min + 1)) + min;
+    return x;
+}
+console.log(getRandom(10,25))
