0923518ffc2ec7186dc345fbf62b77a4c823249d
Joyrup Singh <145583757+Joyrup-singh@users.noreply.github.com>
2023-12-12 16:54:44 +0530

    Add files via upload

---
+++ assg19q5.js
@@ -0,0 +1,12 @@
+// Ascending Order
+var input = 1;
+while (input <= 25) {
+    console.log(input);
+    input++;
+}
+// Descending Order
+var input = 25;
+while (input >= 1) {
+    console.log(input);
+    input--;
+}
